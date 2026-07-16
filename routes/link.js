a75bc1047f3bcf5ccfdeb0a5dbc66d4dd3116a32
ba11iv <basmahalosaimi3@gmail.com>
2026-07-16 13:46:33 +0300

    Fix linkservice import

--- routes/link.js
+++ routes/link.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { analyzeLink } = require("../services/linkService");
+const { analyzeLink } = require("../services/linkservice");
 
 router.post("/", async (req, res) => {
 
